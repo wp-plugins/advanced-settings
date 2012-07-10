e55a242b21cf3bab2503ac60dcc614e07becaf24
webarthur <webarthur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 22:03:29 +0000

    v0.9 beta final
    git-svn-id: https://plugins.svn.wordpress.org/advanced-settings/trunk@570344 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Advanced Settings
 Plugin URI: http://tutzstyle.com/portfolio/advanced-settings/
-Description: Some of settings to WordPress.
-Version: 0.9.3
+Description: Provide some advanced settings that are not provided by WordPress by default
+Version: 0.9.4
 Author: Arthur Araújo
 Author URI: http://tutzstyle.com
 */
@@ -82,7 +82,7 @@ if( $configs['compress'] || $configs['remove_comments'] ) {
 			$code = preg_replace('/<!--(.|\s)*?-->/', '', $code); 
 
 		if( $configs['compress'] )
-			$code = trim_code( $code );
+			$code = trim( preg_replace( '/\s+/', ' ', $code ) );
 
 		/* Acentos */
 		#$code = str_encode( $code );
@@ -113,6 +113,21 @@ var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(
 	}
 }
 
+# Remove admin menu - FIX: IF IS ADMIN USER
+if( $configs['show_query_num'] ) {
+	function __show_sql_query_num(){
+		global $wpdb;
+		$content = '<div style="font-size:10px;text-align:center">'.$wpdb->num_queries.' '.__('SQL queries have been executed to show this page').'</div>';
+        echo $content;
+	}
+	add_action('wp_footer', '__show_sql_query_num');
+}
+
+
+// -----------------------------------------------------------------------
+
+
+
 # MENU
 add_action('admin_menu', 'powerconfigs_menu');
 function powerconfigs_menu() {
@@ -122,87 +137,101 @@ function powerconfigs_menu() {
 # THE PAGE
 function powerconfigs_page() { $configs = get_option('powerconfigs'); ?>
 	
-	<div id="icon-options-general" class="icon32"><br></div>
-	<h2>Advanced settings</h2>
-	
-	<form action="" method="post">
-		
-		<?php wp_nonce_field('pc'); ?>
-		
-<h3 class="title">Header</h3>
-		
-		<label for="remove_menu">
+	<div class="wrap">
 
-			<input name="remove_menu" type="checkbox" id="remove_menu" value="1" <?php if($configs['remove_menu']) echo 'checked="checked"' ?>>
-			Hide admin menu </label>
+		<div style="float:right;width:400px">
+			<div style="float:right; margin-top:10px">
+				 <iframe src="http://www.facebook.com/plugins/like.php?href=<?php echo urlencode('http://tutzstyle.com/') ?>&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="overflow:hidden; width:90px; height:21px; margin:0 0 0 10px; float:right" allowTransparency="true"></iframe> <strong style="line-height:25px;"><?php echo __('Do you like Advanced Settings Plugin? '); ?></strong>
+			</div>
+		</div>
+
+		<div id="icon-options-general" class="icon32"><br></div>
+		<h2>Advanced settings</h2>
 		
-		<!--br />
-		<label for="remove_update_msg">
-			<input name="remove_update_msg" type="checkbox" id="remove_update_msg" value="1" <?php if($configs['remove_update_msg']) echo 'checked="checked"' ?> />
-			<span style="color:red">NÃO FUNCIONA</span> Hide update message from admin</label-->
-		
-		<br />
-		<label for="remove_generator">
-			<input name="remove_generator" type="checkbox" id="remove_generator" value="1" <?php if($configs['remove_generator']) echo 'checked="checked"' ?> />
-			Remove header WordPress generator tag (html)</label>
-		
-		<br />
-		<label for="remove_wlw">
-			<input name="remove_wlw" type="checkbox" id="remove_wlw" value="1" <?php if($configs['remove_wlw']) echo 'checked="checked"' ?> />
-			Remove header WLW Manifest tag (html)</label>
-		
-		<br />
-<h3 class="title">Images</h3>
-		
-		<label for="add_thumbs">
-			<input name="add_thumbs" type="checkbox" id="add_thumbs" value="1" <?php if($configs['add_thumbs']) echo 'checked="checked"' ?> />
-			Add thumbnail support</label>
-		
-		<br />
-		<label for="jpeg_quality">
-			Set JPEG quality to <input name="jpeg_quality" type="text" size="2" maxlength="3" id="jpeg_quality" value="<?php echo (int)$configs['jpeg_quality'] ?>" /> <i style="color:#999">(when send and resize images)</i></label>
-		
-		<!--br />
-<h3 class="title">Contents</h3>
-		
-		<label for="rel_external">
-			<input name="rel_external" type="checkbox" id="rel_external" value="1" <?php if($configs['rel_external']) echo 'checked="checked"' ?> />
-			<span style="color:red">COLOCAR JAVASCRIPT</span> Replaces <span style="color:red">target="_blank"</span> to <span style="color:red">rel="external"</span> <i style="color:#999">(this is for W3C validator, a javascript code replace to target="_blank" again)</i>
-			</label-->
-		
-		<br />
-<h3 class="title">System</h3>
-		
-		<label for="post_type_pag">
-			<input name="post_type_pag" type="checkbox" id="post_type_pag" value="1" <?php if($configs['post_type_pag']) echo 'checked="checked"' ?> />
-			Fix post type pagination
-			</label>
-		
-		<br />
-<h3 class="title">HTML Code output</h3>
-		
-		<label for="compress">
-			<input name="compress" type="checkbox" id="compress" value="1" <?php if($configs['compress']) echo 'checked="checked"' ?> />
-			Compress all code
-			</label>
-		
-		<br />
-		<label for="remove_comments">
-			<input name="remove_comments" type="checkbox" id="remove_comments" value="1" <?php if($configs['remove_comments']) echo 'checked="checked"' ?> />
-			Remove all HTML comments
-			</label>
-		
-		<br />
-<h3 class="title">Footer</h3>
-		
-		<label for="analytics">
-			Google Analytics ID: <input name="analytics" type="text" size="12" id="analytics" value="<?php echo $configs['analytics'] ?>" />
-			<i style="color:#999">(insert a javascript code in the footer)</i>
-			</label>
-		
-		<p class="submit"><input type="submit" name="submit" id="submit" class="button-primary" value="<?= _('Save changes') ?>"></p>
-	</form>
-	
+		<form action="" method="post">
+			
+			<?php wp_nonce_field('pc'); ?>
+			
+	<h3 class="title">Header</h3>
+			
+			<label for="remove_menu">
+
+				<input name="remove_menu" type="checkbox" id="remove_menu" value="1" <?php if($configs['remove_menu']) echo 'checked="checked"' ?>>
+				Hide admin menu </label>
+			
+			<!--br />
+			<label for="remove_update_msg">
+				<input name="remove_update_msg" type="checkbox" id="remove_update_msg" value="1" <?php if($configs['remove_update_msg']) echo 'checked="checked"' ?> />
+				<span style="color:red">NÃO FUNCIONA</span> Hide update message from admin</label-->
+			
+			<br />
+			<label for="remove_generator">
+				<input name="remove_generator" type="checkbox" id="remove_generator" value="1" <?php if($configs['remove_generator']) echo 'checked="checked"' ?> />
+				Remove header WordPress generator tag (html)</label>
+			
+			<br />
+			<label for="remove_wlw">
+				<input name="remove_wlw" type="checkbox" id="remove_wlw" value="1" <?php if($configs['remove_wlw']) echo 'checked="checked"' ?> />
+				Remove header WLW Manifest tag (html)</label>
+			
+			<br />
+	<h3 class="title">Images</h3>
+			
+			<label for="add_thumbs">
+				<input name="add_thumbs" type="checkbox" id="add_thumbs" value="1" <?php if($configs['add_thumbs']) echo 'checked="checked"' ?> />
+				Add thumbnail support</label>
+			
+			<br />
+			<label for="jpeg_quality">
+				Set JPEG quality to <input name="jpeg_quality" type="text" size="2" maxlength="3" id="jpeg_quality" value="<?php echo (int)$configs['jpeg_quality'] ?>" /> <i style="color:#999">(when send and resize images)</i></label>
+			
+			<!--br />
+	<h3 class="title">Contents</h3>
+			
+			<label for="rel_external">
+				<input name="rel_external" type="checkbox" id="rel_external" value="1" <?php if($configs['rel_external']) echo 'checked="checked"' ?> />
+				<span style="color:red">COLOCAR JAVASCRIPT</span> Replaces <span style="color:red">target="_blank"</span> to <span style="color:red">rel="external"</span> <i style="color:#999">(this is for W3C validator, a javascript code replace to target="_blank" again)</i>
+				</label-->
+			
+			<br />
+	<h3 class="title">System</h3>
+			
+			<label for="post_type_pag">
+				<input name="post_type_pag" type="checkbox" id="post_type_pag" value="1" <?php if($configs['post_type_pag']) echo 'checked="checked"' ?> />
+				Fix post type pagination
+				</label>
+			
+			<br />
+	<h3 class="title">HTML Code output</h3>
+			
+			<label for="compress">
+				<input name="compress" type="checkbox" id="compress" value="1" <?php if($configs['compress']) echo 'checked="checked"' ?> />
+				Compress all code
+				</label>
+			
+			<br />
+			<label for="remove_comments">
+				<input name="remove_comments" type="checkbox" id="remove_comments" value="1" <?php if($configs['remove_comments']) echo 'checked="checked"' ?> />
+				Remove all HTML comments
+				</label>
+			
+			<br />
+	<h3 class="title">Footer</h3>
+			
+			<label for="show_query_num">
+				<input name="show_query_num" type="checkbox" id="show_query_num" value="1" <?php if($configs['show_query_num']) echo 'checked="checked"' ?> />
+				Display total number of executed SQL queries
+				</label>
+			
+			<br />
+			<label for="analytics">
+				Google Analytics ID: <input name="analytics" type="text" size="12" id="analytics" value="<?php echo $configs['analytics'] ?>" />
+				<i style="color:#999">(inserts a javascript code in the footer)</i>
+				</label>
+			
+			<p class="submit"><input type="submit" name="submit" id="submit" class="button-primary" value="<?= _('Save changes') ?>"></p>
+		</form>
+	</div>
 	<?
 }
 
--- readme.txt
+++ readme.txt
@@ -1,15 +1,38 @@
 === Advanced Settings ===
 Contributors: webarthur
-Tags: settings
-Requires at least: 3.4.1
+Tags: settings, option, admin, menu, page, image, setting, images, google, analytics, compress, html, thumbnail, post type
+Requires at least: 3.0
 Tested up to: 3.4.1
-Stable tag: 0.9.3
+Stable tag: 0.9.4
 License: GPLv2 or later
-
-Using WordPress as a Book-Writing Platform.
+Provide some advanced settings that are not provided by WordPress by default
 
 == Description ==
 
-Advanced Settings
+This is an essential plugin for your WordPress websites:
+
+* Hide top WP admin bar
+* Remove HTML comments
+* Compact HTML code
+* Insert google analytics code
+* Remove header WordPress generator tag
+* Remove header WLW Manifest tag
+* Display total number of executed SQL queries
+* Add thumbnail support
+* Set JPEG quality
+* Fix post type pagination 
+* Display total number of executed SQL queries
+* and more to come ...
+
+
+== Installation ==
+
+Upload plugin to your blog, activate it, then click on the Settings > Advanced on the menu.
+
+
+== Screenshots ==
+
+1. screenshot-1.png 
+2. screenshot-2.png 
 
 
